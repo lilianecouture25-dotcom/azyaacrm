9513a641ce067f259df5f0d8bc706e692e199bf3
houcemata <houcemamata@gmail.com>
2026-07-26 19:19:45 +0100

    Expose Noest shipping API routes

---
+++ api/ship-order.js
@@ -0,0 +1,4 @@
+// Public Vercel route for the single-order Noest handler.
+import handler from "./_ship-order.js";
+
+export default handler;
---
+++ api/ship-orders-bulk.js
@@ -0,0 +1,4 @@
+// Public Vercel route for the bulk Noest handler.
+import handler from "./_ship-orders-bulk.js";
+
+export default handler;
